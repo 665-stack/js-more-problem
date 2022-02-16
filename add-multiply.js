eb040f6d198a4cb226142a85c3479627e8d70acb
unknown <mahmodulshahin665@gmail.com>
2022-02-16 09:05:59 +0600

    add more js code

--- add-multiply.js
+++ add-multiply.js
@@ -5,16 +5,16 @@
   bedWodd = 50cft
  */
 function woodCalculator(chairQuantity, tableQuantity, bedQuantity) {
-    const perChairWood = 3;
-    const perTableWood = 10;
-    const perBedWood = 50;
-    // calculation
-    const chairWoodQuantity = chairQuantity * perChairWood;
-    const tableWoodQuantity = tableQuantity * perTableWood;
-    const bedWoodQuantity = bedQuantity * perBedWood;
-    // total
-    const totalWood = chairWoodQuantity + tableWoodQuantity + bedWoodQuantity;
-    return totalWood;
+  const perChairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
+  // calculation
+  const chairWoodQuantity = chairQuantity * perChairWood;
+  const tableWoodQuantity = tableQuantity * perTableWood;
+  const bedWoodQuantity = bedQuantity * perBedWood;
+  // total
+  const totalWood = chairWoodQuantity + tableWoodQuantity + bedWoodQuantity;
+  return totalWood;
 }
-const totalWoodResult = woodCalculator(1, 1, o);
-console.log('total wood result is :', totalWoodResult);
+const totalWoodResult = woodCalculator(4, 2, 2);
+console.log('total wood quantity is :', totalWoodResult);
